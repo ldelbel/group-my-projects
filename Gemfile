12b7975816c552239d6da495c2b5e0068c6da545
ldelbel <lucdelbel@gmail.com>
2020-09-28 09:57:11 -0300

    Add Shrine gem and User model

--- Gemfile
+++ Gemfile
@@ -21,9 +21,8 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
 # Use Active Storage variant
-# gem 'image_processing', '~> 1.2'
+gem 'image_processing', '~> 1.12'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
@@ -52,3 +51,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 # gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'shrine', '~> 3.2', '>= 3.2.2'
+gem 'mini_magick', '~> 4.10', '>= 4.10.1'
---
+++ config/initializers/shrine.rb
@@ -0,0 +1,21 @@
+require "shrine"
+require "shrine/storage/file_system"
+require "shrine/storage/memory"
+
+if Rails.env.test?
+  Shrine.storages = { 
+    cache: Shrine::Storage::Memory.new # temporary 
+    store: Shrine::Storage::Memory.new # permanent 
+  }
+else
+  Shrine.storages = { 
+    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary 
+    store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent 
+  }
+end
+
+Shrine.plugin :activerecord 
+Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays 
+Shrine.plugin :restore_cached_data # re-extract metadata when attaching a cached file 
+Shrine.plugin :validation
+Shrine.plugin :validation_helpers
