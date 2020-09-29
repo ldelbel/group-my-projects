ed0ff69f71fafe43a0d30b90acad6d8d9aafea1b
ldelbel <lucdelbel@gmail.com>
2020-09-29 14:20:40 -0300

    Add styling to login_path and signup_path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def home?
+    request.url == root_url
+  end
+
+  def login_page?
+    request.url == login_url
+  end
+
+  def signup_page?
+    request.url == signup_url
+  end
 end
